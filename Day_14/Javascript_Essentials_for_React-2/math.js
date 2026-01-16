a6954be565b3b92daac8907116a9a7e4d0f2ba96
Dheeraj <dheerajchavhan15@gmail.com>
2026-01-17 02:01:10 +0530

    message

---
+++ Day_14/Javascript_Essentials_for_React-2/math.js
@@ -0,0 +1,12 @@
+// Export the function
+export function addition(a,b,c){
+    console.log(a+b+c)
+}
+export function multiply(a,b){
+    console.log(a*b)
+}
+
+// Default import and export --> Only one default export per file
+export default function divide(a,b){
+    console.log(a/b)
+}
