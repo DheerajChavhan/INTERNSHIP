3a32d38a5684a87137e52216e7e821b12271d80b
Dheeraj <dheerajchavhan15@gmail.com>
2026-02-10 02:39:46 +0530

    Day_11&12

--- Day_11/Blog_Management_System/main.py
+++ Day_11/Blog_Management_System/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from db.database import engine,Base
-from routers import blog
+from routers import blog,AIChat
 
 app=FastAPI(
     title="Blog Management System"
@@ -8,6 +8,7 @@ app=FastAPI(
 
 Base.metadata.create_all(bind=engine)
 app.include_router(blog.router)
+app.include_router(AIChat.router)
 
 @app.get("/")
 def root():
